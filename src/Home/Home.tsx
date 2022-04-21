b0b5d25fbae5d39cd940907e96faaf1da69459ad
mdshohidulislam1486 <Shohidul Islam>
2022-04-21 21:36:55 +0600

    updated

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,16 +1,32 @@
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Container, Divider, IconButton, InputBase, Paper, Typography } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
 import React from 'react'
 import './Home.css'
 
 const Home = () => {
+
+
+
   return (
     <div>
         <Container>
-             <Typography sx={{fontWeight:600, textAlign:'center'}} variant='h2' component='h2'>Country Detail </Typography>
+             <Typography sx={{fontWeight:600, textAlign:'center', py:3}} variant='h2' component='h2'>Country Detail </Typography>
              <Typography sx={{textAlign:'center'}} variant='body1' component='body'>We made it easy for you to know more about your favorite countries!!!</Typography>
 
-             <Box>
-                 
+             <Box sx={{display:'flex', justifyContent:'center', my:5}}>
+                <Paper
+                        component="form"
+                        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+                        >
+                        <InputBase
+                            sx={{ ml: 1, flex: 1 }}
+                            placeholder="Search your favorite country"
+                            inputProps={{ 'aria-label': 'Search your favorite country' }}
+                        />
+                        <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                            <SearchIcon />
+                        </IconButton>
+                    </Paper>
              </Box>
         </Container>
         
